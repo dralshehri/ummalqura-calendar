5fc63cefd611269d918c5ea56ae7e54c8a1ce572
dralshehri <dralshehri@users.noreply.github.com>
2021-05-30 22:11:18 +0300

    Update `generate` script

--- scripts/generate.py
+++ scripts/generate.py
@@ -1,10 +1,11 @@
 import csv
 from datetime import date, timedelta
-from hijri_converter import convert, ummalqura
+from hijri_converter.convert import Gregorian
+from hijri_converter.ummalqura import GREGORIAN_RANGE
 
 
 def generate_calendar(filename):
-    start, end = ummalqura.gregorian_range
+    start, end = GREGORIAN_RANGE
     start_date = date(*start)
     end_date = date(*end)
 
@@ -14,11 +15,11 @@ def generate_calendar(filename):
         row_date = start_date
         while row_date <= end_date:
             gy, gm, gd = row_date.timetuple()[:3]
-            hy, hm, hd = convert.Gregorian(gy, gm, gd).to_hijri().datetuple()
+            hy, hm, hd = Gregorian(gy, gm, gd).to_hijri().datetuple()
             rjd = row_date.toordinal() + 1721425 - 2400000
             calendar_writer.writerow([rjd, hy, hm, hd, gy, gm, gd])
             row_date += timedelta(days=1)
 
 
 if __name__ == '__main__':
-    generate_calendar("../data/ummalqura-calendar2.csv")
+    generate_calendar("../data/ummalqura-calendar.csv")
